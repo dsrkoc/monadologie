5a1e3950376bc91799f2a6e3c23991a11aceb453
Dinko Srkoč <dinko.srkoc@helix.hr>
2011-03-17 18:45:46 +0100

    Added example that uses Option monad.

---
+++ Example2.groovy
@@ -0,0 +1,28 @@
+import hr.helix.monadologie.*
+import static hr.helix.monadologie.MonadComprehension.foreach
+import hr.helix.monadologie.monads.Option
+import static hr.helix.monadologie.monads.Option.Some
+import static hr.helix.monadologie.monads.Option.None
+
+
+class Order { Option<LineItem> lineItem }
+class LineItem { Option<Product> product }
+class Product { String name }
+
+def prod =
+    /*None*/
+    Some(new Product(name:'a new product'))
+def maybeOrder = new Order(
+        lineItem: Some(
+            new LineItem( product: prod)
+        )
+)
+/*maybeOrder.lineItem = None*/
+
+res = foreach {
+    order    = takeFrom { Some(maybeOrder) }
+    lineItem = takeFrom { order.lineItem }
+    product  = takeFrom { lineItem.product }
+    yield { product.name }
+}
+println "example2: res=$res"
