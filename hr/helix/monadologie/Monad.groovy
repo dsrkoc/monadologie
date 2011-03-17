164f1dea98c69ce45cc1899b6e8cd7c32c31489f
Dinko Srkoč <dinko.srkoc@helix.hr>
2011-03-17 12:30:58 +0100

    Introduced a Monad interface.
    At the moment nobody uses it, but this might change.

---
+++ hr/helix/monadologie/Monad.groovy
@@ -0,0 +1,18 @@
+package hr.helix.monadologie
+
+interface Monad<M> {       // ... zapravo, Monad[M[A]]
+
+    /* param   value of type A
+     * returns container of type A
+     *
+     * unit :: a -> M a */
+    M unit(Object a)       // ... unit(A): M[A]
+
+    /* param   m ... container of type A
+     * param   f ... transformer function (A => M[B])
+     * returns container of type B
+     *
+     * bind :: M a -> (a -> M b) -> M b */
+    /*M bind(M m, Closure f) // ... bind[B](M[A], A => M[B]): M[B]*/
+    M bind(Closure f) // ... bind[B](M[A], A => M[B]): M[B]
+}
