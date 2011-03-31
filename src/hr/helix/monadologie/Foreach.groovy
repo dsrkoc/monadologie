1676abeaf331857b12cb7852fe4af11caa821f15
Dinko Srkoč <dinko.srkoc@helix.hr>
2011-03-31 11:52:17 +0200

    Removed files that doesn't really belong to the project.
    Those files are either deprecated or examples of usage (in which case
    should be replaced with tests).

--- src/hr/helix/monadologie/Foreach.groovy
+++
@@ -1,9 +0,0 @@
-package hr.helix.monadologie
-
-class Foreach {
-    static foreach(Closure action) {
-        action.delegate = new Each()
-        action.resolveStrategy = Closure.DELEGATE_ONLY
-        action()
-    }
-}
